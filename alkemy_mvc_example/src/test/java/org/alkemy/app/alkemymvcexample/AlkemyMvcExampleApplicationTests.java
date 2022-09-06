776e3e76332933c8e0ae26a9b6a2427c02449269
Emilio Watemberg <emilio.watemberg@gmail.com>
2022-09-06 07:49:42 -0300

    Primer commit

---
+++ alkemy_mvc_example/src/test/java/org/alkemy/app/alkemymvcexample/AlkemyMvcExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alkemy.app.alkemymvcexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkemyMvcExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
